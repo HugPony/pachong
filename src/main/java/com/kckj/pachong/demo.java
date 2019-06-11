66d8f8e3aec325791559417b94364439c9727b9d
小宇 <1205322743@qq.com>
2019-06-11 12:36:04 +0800

    Initial commit

---
+++ src/main/java/com/kckj/pachong/demo.java
@@ -0,0 +1,20 @@
+package com.kckj.pachong;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class demo {
+    public static void main(String[] args) throws Exception{
+
+        //获取到网页元素 可通过Jsoup api进行操作
+        Document document = Jsoup.connect("https://www.baidu.com").get();
+        System.out.println(document);
+
+    }
+
+}
